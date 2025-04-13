3ec7356a49a3c83f15459e087c143233396c54fe
dakorot <koropenko.daria>
2025-04-13 17:47:20 +0200

    Clock abstract class

--- Clock.java
+++ Clock.java
@@ -1,2 +1,28 @@
+import java.time.LocalTime;
+
 public abstract class Clock {
+    int hour, minute, second;
+
+    public void setCurrentTime() {
+        LocalTime currentTime = LocalTime.now();
+        this.hour = currentTime.getHour();
+        this.minute = currentTime.getMinute();
+        this.second = currentTime.getSecond();
+    };
+
+    public void setTime(int hour, int minute, int second) {
+        try {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        catch (Exception IllegalArgumentException) {
+            System.err.println(IllegalArgumentException.getMessage());
+        }
+    };
+
+    public String toString() {
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
 }
