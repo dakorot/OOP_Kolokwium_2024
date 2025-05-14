8107f1fe570691ab2859ea3cee768bc0c4eec15d
Daria <koropenko.daria@gmail.com>
2025-05-14 20:24:40 +0200

    Update to City + test in Main

--- City.java
+++ City.java
@@ -6,29 +6,26 @@ import java.util.Scanner;
 public class City {
     String name;
     String timezone;
-    String longitude;
-    String latitude;
+    String latitude; // szerokość - pionowo
+    String longitude; // dlugość - poziomo
 
-    private City parseLine(String line) {
+    private static City parseLine(String line) {
         City newCity = new City();
         String[] values = line.split(",");
         newCity.name = values[0];
         newCity.timezone = values[1];
-        newCity.longitude = values[2];
-        newCity.latitude = values[3];
+        newCity.latitude = values[2];
+        newCity.longitude = values[3];
 
         return newCity;
     }
 
-    public HashMap<String, String> parseFile(String path) throws FileNotFoundException {
-        HashMap<String, String> cities = new HashMap<>();
+    public static HashMap<String, City> parseFile(String path) throws FileNotFoundException {
+        HashMap<String, City> cities = new HashMap<>();
         try (Scanner scanner = new Scanner(new File(path))) {
             while (scanner.hasNextLine()) {
                 City city = parseLine(scanner.nextLine());
-                cities.put("name", city.name);
-                cities.put("timezone", city.timezone);
-                cities.put("longitude", city.longitude);
-                cities.put("latitude", city.latitude);
+                cities.put(city.name, city);
             }
 
             return cities;
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         DigitalClock testClock = new DigitalClock(11, 32, 1, 24);
-
         System.out.println(testClock.toString());
+
+        String filePath = "./strefy.csv";
+        HashMap<String, City> parsedCities = City.parseFile(filePath);
     }
 }
